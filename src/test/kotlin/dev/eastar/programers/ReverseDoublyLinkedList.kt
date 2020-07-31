774219dd1e08534125c8601fa10de588c8eab2ed
pluto <pluto@eastandroid.com>
2020-07-31 18:01:22 +0900

    ReverseDoublyLinkedList

---
+++ src/test/kotlin/dev/eastar/programers/ReverseDoublyLinkedList.kt
@@ -0,0 +1,122 @@
+package dev.eastar.programers
+
+import dev.eastar.BaseLog
+import org.junit.Test
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+class ReverseDoublyLinkedList : BaseLog(){
+    @Test
+    fun test(){
+        main("1\n" +
+    "4\n" +
+    "1\n" +
+    "2\n" +
+    "3\n" +
+    "4")
+    }
+}
+
+// Complete the reverse function below.
+
+/*
+ * For your reference:
+ *
+ * DoublyLinkedListNode {
+ *     data: Int
+ *     next: DoublyLinkedListNode
+ *     prev: DoublyLinkedListNode
+ * }
+ *
+ */
+fun reverse(llist: DoublyLinkedListNode?): DoublyLinkedListNode? {
+    return llist
+}
+
+
+class DoublyLinkedListNode(nodeData: Int) {
+    public var data: Int
+    public var next: DoublyLinkedListNode?
+    public var prev: DoublyLinkedListNode?
+
+    init {
+        data = nodeData
+        next = null
+        prev = null
+    }
+}
+
+class DoublyLinkedList {
+    public var head: DoublyLinkedListNode?
+    public var tail: DoublyLinkedListNode?
+
+    init {
+        head = null
+        tail = null
+    }
+
+    public fun insertNode(nodeData: Int) {
+        var node = DoublyLinkedListNode(nodeData)
+
+        if (head == null) {
+            head = node
+        } else {
+            tail?.next = node
+            node?.prev = tail
+        }
+
+        tail = node
+    }
+
+}
+
+fun printDoublyLinkedList(head: DoublyLinkedListNode?, sep: String) {
+    var node = head;
+
+    while (node != null) {
+        print(node?.data)
+        node = node?.next
+
+        if (node != null) {
+            print(sep)
+        }
+    }
+}
+
+
+
+fun main(input : String) {
+    val scan = Scanner(StringReader(input))
+
+    val t = scan.nextLine().trim().toInt()
+
+    for (tItr in 1..t) {
+        val llistCount = scan.nextLine().trim().toInt()
+        val llist = DoublyLinkedList()
+
+        for (i in 0 until llistCount) {
+            val llist_item = scan.nextLine().trim().toInt()
+            llist.insertNode(llist_item)
+        }
+
+        val llist1 = reverse(llist?.head)
+
+        printDoublyLinkedList(llist1, " ")
+    }
+}
